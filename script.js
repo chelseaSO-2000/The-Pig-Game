6349925857a615fbeafbc4f98cbd8892979aab02
Wing Chi So <wingchiso@Wings-MacBook-Pro-3.local>
2023-10-03 09:03:45 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const btnNewGameEl = document.querySelector('.btn--new');
+
+let scores, currentScore, activePlayer, playing;
+
+const resetAll = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+};
+// Starting conditions
+resetAll();
+
+const switchPlayer = function () {
+  // Switch to next player
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+btnNewGameEl.addEventListener('click', resetAll);
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Check for rolled 1: if true, switch to next player
+
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+// Define the function
+function recur(n, cur) {
+  if (!cur) {
+    cur = 0;
+  }
+  if (n < 2) {
+    throw new Error('Invalid input');
+  }
+  if (n === 2) {
+    return 1 / n + cur;
+  }
+  return recur(n - 1, cur + 1 / (n * (n - 1)));
+}
+
+// Call the function with your desired inputs
+const n = 10; // You can change this to any positive integer
+const cur = 0;
+const result = recur(n, cur);
+
+console.log(result);
